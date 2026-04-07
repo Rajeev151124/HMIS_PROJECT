e46228ff4972d45e0b8e10ec22637fa9a7f9210a
Ubuntu <ubuntu@ip-172-31-2-143.ap-south-1.compute.internal>
2026-04-07 17:06:01 +0000

    5th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,23 @@
-import React, { useState } from "react";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [patient, setPatient] = useState({
-    name: "",
-    age: "",
-    disease: ""
-  });
-
-  const register = async () => {
-    await axios.post("http://<EC2-IP>:30007/patients", patient);
-    alert("Patient saved!");
-  };
-
   return (
-    <div style={{ padding: 30 }}>
-      <h1>HMIS System</h1>
-
-      <h2>Register Patient</h2>
-
-      <input placeholder="Name"
-        onChange={e => setPatient({...patient, name:e.target.value})} />
-
-      <input placeholder="Age"
-        onChange={e => setPatient({...patient, age:e.target.value})} />
-
-      <input placeholder="Disease"
-        onChange={e => setPatient({...patient, disease:e.target.value})} />
-
-      <button onClick={register}>Submit</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
